92083f664ba275f2c10e296e286c1e65726231ed
Vlad <vladandryschenko98@gmail.com>
2024-09-11 20:55:32 +0300

    App: add init Appodeal

---
+++ app/src/main/java/com/divinee/puwer/App.kt
@@ -0,0 +1,12 @@
+package com.divinee.puwer
+
+import android.app.Application
+import com.appodeal.ads.Appodeal
+
+class App : Application() {
+    private val applicationID = "29c19453396a950cad4f196a1239e03a595c2794f3148775"
+    override fun onCreate() {
+        super.onCreate()
+        Appodeal.initialize(this, applicationID, Appodeal.INTERSTITIAL)
+    }
+}

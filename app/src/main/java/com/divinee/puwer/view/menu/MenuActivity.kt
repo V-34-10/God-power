0a4eef4ba094cb1babe010d37a47b4a0664d66ac
Vlad <vladandryschenko98@gmail.com>
2024-09-12 17:57:57 +0300

    MenuActivity: fix setContentView

--- app/src/main/java/com/divinee/puwer/view/menu/MenuActivity.kt
+++ app/src/main/java/com/divinee/puwer/view/menu/MenuActivity.kt
@@ -20,7 +20,7 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

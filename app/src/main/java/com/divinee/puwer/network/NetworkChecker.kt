afb404483d1c8a54972e632216daed4833dae1b5
Vlad <vladandryschenko98@gmail.com>
2024-09-13 19:06:38 +0300

    NetworkChecker: add checkEthernetStatus

---
+++ app/src/main/java/com/divinee/puwer/network/NetworkChecker.kt
@@ -0,0 +1,14 @@
+package com.divinee.puwer.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+object NetworkChecker {
+
+    fun checkEthernetStatus(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        return activeNetworkInfo?.isConnectedOrConnecting == true
+    }
+}

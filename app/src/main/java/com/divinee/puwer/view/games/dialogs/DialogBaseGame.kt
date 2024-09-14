1371f3e34ddfd27cacd25a9280b2fad2050c8e6d
Vlad <vladandryschenko98@gmail.com>
2024-09-14 19:14:45 +0300

    DialogBaseGame: add runDialogLoseGame, runDialogVictoryGame

---
+++ app/src/main/java/com/divinee/puwer/view/games/dialogs/DialogBaseGame.kt
@@ -0,0 +1,36 @@
+package com.divinee.puwer.view.games.dialogs
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.Window
+import android.widget.ImageView
+import com.divinee.puwer.R
+
+object DialogBaseGame {
+
+    fun runDialogLoseGame(context: Context) {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_game_lose)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setCanceledOnTouchOutside(false)
+
+        val btnRestart = dialog.findViewById<ImageView>(R.id.btn_restart_dialog)
+        btnRestart.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+    }
+
+    fun runDialogVictoryGame(context: Context) {
+        val dialog = Dialog(context)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_game_victory)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setCanceledOnTouchOutside(false)
+
+        val btnRestart = dialog.findViewById<ImageView>(R.id.btn_next_dialog)
+        btnRestart.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+    }
+}

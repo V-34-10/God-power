de9cfdb4b4abb5c1cb81c1a8e60325284cb2edc2
Vlad <vladandryschenko98@gmail.com>
2024-09-16 13:04:10 +0300

    SceneActivity: fix id layout

--- app/src/main/java/com/divinee/puwer/view/games/SceneActivity.kt
+++ app/src/main/java/com/divinee/puwer/view/games/SceneActivity.kt
@@ -20,7 +20,7 @@ class SceneActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainScene)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets

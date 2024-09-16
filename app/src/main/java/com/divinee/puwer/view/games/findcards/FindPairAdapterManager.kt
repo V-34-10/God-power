0c031545cd8068d0f54fdfa6c69a669e3db48f57
Vlad <vladandryschenko98@gmail.com>
2024-09-17 00:49:47 +0300

    FindPairAdapterManager: add initRecyclerView, setupAdapter, resetAdapter, notifyItemChanged, setOnItemClickListener

---
+++ app/src/main/java/com/divinee/puwer/view/games/findcards/FindPairAdapterManager.kt
@@ -0,0 +1,36 @@
+package com.divinee.puwer.view.games.findcards
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.divinee.puwer.adapters.FindCardAdapter
+import com.divinee.puwer.models.FindCard
+
+class FindPairAdapterManager(
+    private val bindingSetup: BindingSetup
+) {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: FindCardAdapter
+
+    fun initRecyclerView(context: Context) {
+        recyclerView = bindingSetup.setupRecyclerView(context)
+    }
+
+    fun setupAdapter(cardList: List<FindCard>) {
+        adapter = FindCardAdapter(cardList)
+        recyclerView.adapter = adapter
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun resetAdapter() {
+        adapter.notifyDataSetChanged()
+    }
+
+    fun notifyItemChanged(position: Int) {
+        adapter.notifyItemChanged(position)
+    }
+
+    fun setOnItemClickListener(onClick: (FindCard, Int) -> Unit) {
+        adapter.onFindCardClick = onClick
+    }
+}

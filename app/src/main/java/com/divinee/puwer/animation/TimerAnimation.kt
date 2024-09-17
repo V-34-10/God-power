8c59d5f35c599fee873393c525e0bde77a7af6ee
Vlad <vladandryschenko98@gmail.com>
2024-09-17 20:51:11 +0300

    TimerAnimation: optimization: startTimer/updateLineTimerWidth/stopTimer/resetTimer/resetLineTimerWidth -> for use TimerAnimation in PuzzleGame

--- app/src/main/java/com/divinee/puwer/animation/TimerAnimation.kt
+++ app/src/main/java/com/divinee/puwer/animation/TimerAnimation.kt
@@ -3,8 +3,14 @@ package com.divinee.puwer.animation
 import android.animation.ValueAnimator
 import android.content.Context
 import android.os.CountDownTimer
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity.MODE_PRIVATE
+import androidx.viewbinding.ViewBinding
+import com.divinee.puwer.R
 import com.divinee.puwer.databinding.FragmentFindCardsGameBinding
+import com.divinee.puwer.databinding.FragmentPuzzleGameBinding
 import com.divinee.puwer.view.games.dialogs.DialogBaseGame.runDialogLoseGame
+import com.divinee.puwer.view.games.dialogs.DialogBaseGame.runDialogLoseGamePuzzle
 import com.divinee.puwer.view.games.findcards.FindPairGameManager
 
 class TimerAnimation {
@@ -16,61 +22,102 @@ class TimerAnimation {
     private var initialWidth = 0
 
     fun startTimer(
-        binding: FragmentFindCardsGameBinding,
+        binding: ViewBinding,
         context: Context,
-        gameManager: FindPairGameManager
+        gameManager: FindPairGameManager?
     ) {
         if (initialWidth == 0) {
-            initialWidth = binding.timerCounter.lineTimer.width
+            when (binding) {
+                is FragmentFindCardsGameBinding -> initialWidth =
+                    binding.timerCounter.lineTimer.width
+
+                is FragmentPuzzleGameBinding -> initialWidth = binding.timerCounter.lineTimer.width
+            }
         }
         startTime = System.currentTimeMillis() - elapsedTime
         timer = object : CountDownTimer(animationDuration, delayTimer) {
             override fun onTick(millisUntilFinished: Long) {
                 elapsedTime = System.currentTimeMillis() - startTime
-                updateLineTimerWidth(millisUntilFinished, binding)
+                when (binding) {
+                    is FragmentFindCardsGameBinding -> updateLineTimerWidth(
+                        millisUntilFinished,
+                        binding
+                    )
+
+                    is FragmentPuzzleGameBinding -> updateLineTimerWidth(
+                        millisUntilFinished,
+                        binding
+                    )
+                }
             }
 
             override fun onFinish() {
-                runDialogLoseGame(context, gameManager)
-            }
-        }.start()
-    }
+                when (binding) {
+                    is FragmentFindCardsGameBinding -> runDialogLoseGame(context, gameManager)
+                    is FragmentPuzzleGameBinding ->
+                        runDialogLoseGamePuzzle(
+                            context,
+                            this@TimerAnimation,
+                            binding,
+                            context.getSharedPreferences("PrefDivinePower", MODE_PRIVATE)
+                                .getString("levelGame", context.getString(R.string.easy_level_btn))
+                                .toString()
+                        )
+                    }
+                }
+            }.start()
+        }
 
     private fun updateLineTimerWidth(
         millisUntilFinished: Long,
-        binding: FragmentFindCardsGameBinding
+        binding: ViewBinding
     ) {
-        val lineTimer = binding.timerCounter.lineTimer
+        val lineTimer: View? = when (binding) {
+            is FragmentFindCardsGameBinding -> binding.timerCounter.lineTimer
+            is FragmentPuzzleGameBinding -> binding.timerCounter.lineTimer
+            else -> null
+        }
+
         val newWidth = (millisUntilFinished / animationDuration.toFloat() * initialWidth).toInt()
 
-        val animation = ValueAnimator.ofInt(lineTimer.width, newWidth)
-        animation.duration = delayTimer
-        animation.addUpdateListener {
+        val animation = lineTimer?.width?.let { ValueAnimator.ofInt(it, newWidth) }
+        animation?.duration = delayTimer
+        animation?.addUpdateListener {
             val value = it.animatedValue as Int
             val layoutParams = lineTimer.layoutParams
-            layoutParams.width = value
+            layoutParams?.width = value
             lineTimer.layoutParams = layoutParams
         }
-        animation.start()
+        animation?.start()
     }
 
     fun stopTimer(
-        binding: FragmentFindCardsGameBinding
+        binding: ViewBinding
     ) {
         timer?.cancel()
-        resetTimer(binding)
+        when (binding) {
+            is FragmentFindCardsGameBinding -> resetTimer(binding)
+            is FragmentPuzzleGameBinding -> resetTimer(binding)
+        }
     }
 
-    private fun resetTimer(binding: FragmentFindCardsGameBinding) {
+    private fun resetTimer(binding: ViewBinding) {
         startTime = 0
         elapsedTime = 0
-        resetLineTimerWidth(binding)
+        when (binding) {
+            is FragmentFindCardsGameBinding -> resetLineTimerWidth(binding)
+            is FragmentPuzzleGameBinding -> resetLineTimerWidth(binding)
+        }
     }
 
-    private fun resetLineTimerWidth(binding: FragmentFindCardsGameBinding) {
-        val lineTimer = binding.timerCounter.lineTimer
-        val layoutParams = lineTimer.layoutParams
-        layoutParams.width = initialWidth
-        lineTimer.layoutParams = layoutParams
+    private fun resetLineTimerWidth(binding: ViewBinding) {
+        val lineTimer: View? = when (binding) {
+            is FragmentFindCardsGameBinding -> binding.timerCounter.lineTimer
+            is FragmentPuzzleGameBinding -> binding.timerCounter.lineTimer
+            else -> null
+        }
+        val layoutParams = lineTimer?.layoutParams
+        layoutParams?.width = initialWidth
+        lineTimer?.layoutParams = layoutParams
     }
 }

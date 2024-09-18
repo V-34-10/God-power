d5e8af4e4a8bcbac6871ba52e6bb44d59364b0db
Vlad <vladandryschenko98@gmail.com>
2024-09-19 00:24:45 +0300

    DailyActivity: fix add dailyClickDaysButtons, wasRewardShownToday/markRewardShownToday, checkDailyReward, resetRewardProgress, loadRewardProgress, updateButtonState, collectReward, goToMenu, onBackPressed

--- app/src/main/java/com/divinee/puwer/view/daily/DailyActivity.kt
+++ app/src/main/java/com/divinee/puwer/view/daily/DailyActivity.kt
@@ -1,105 +1,189 @@
 package com.divinee.puwer.view.daily
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.ImageView
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.divinee.puwer.R
 import com.divinee.puwer.databinding.ActivityDailyBinding
+import com.divinee.puwer.view.games.findcards.bonusgame.BonusWheelGame.stringToNumber
 import com.divinee.puwer.view.menu.MenuActivity
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class DailyActivity : AppCompatActivity() {
     private val binding by lazy { ActivityDailyBinding.inflate(layoutInflater) }
-    private lateinit var dailyRewardManager: DailyRewardManager
-    private lateinit var btnFirstDay: ImageView
-    private lateinit var btnSecondDay: ImageView
-    private lateinit var btnThreeDay: ImageView
+    private lateinit var sharedPreferences: SharedPreferences
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        dailyRewardManager = DailyRewardManager(this)
-        dailyRewardManager.checkAndResetProgress()
-
-        btnFirstDay = findViewById(R.id.btn_first_day)
-        btnSecondDay = findViewById(R.id.btn_second_day)
-        btnThreeDay = findViewById(R.id.btn_three_day)
-
-        updateButtonStates()
-
-        btnFirstDay.setOnClickListener {
-            claimReward(1)
+        sharedPreferences = getSharedPreferences("PrefDivinePower", Context.MODE_PRIVATE)
+        if (wasRewardShownToday()) {
+            goToMenu()
+            return
         }
 
-        btnSecondDay.setOnClickListener {
-            claimReward(2)
+        checkDailyReward()
+        loadRewardProgress()
+        dailyClickDaysButtons()
+    }
+
+    private fun dailyClickDaysButtons() {
+        binding.btnFirstDay.setOnClickListener {
+            collectReward(1, binding.btnFirstDay)
         }
 
-        btnThreeDay.setOnClickListener {
-            claimReward(3)
+        binding.btnSecondDay.setOnClickListener {
+            collectReward(2, binding.btnSecondDay)
+        }
+
+        binding.btnThreeDay.setOnClickListener {
+            collectReward(3, binding.btnThreeDay)
         }
     }
 
-    private fun updateButtonStates() {
-        val currentDay = dailyRewardManager.getCurrentDay()
+    private fun wasRewardShownToday(): Boolean =
+        sharedPreferences.getString("lastRewardShownDate", null) == dateFormat.format(Date())
 
-        btnFirstDay.isEnabled = currentDay >= 1 && !dailyRewardManager.isRewardClaimedForDay(1)
-        btnSecondDay.isEnabled = currentDay >= 2 && !dailyRewardManager.isRewardClaimedForDay(2)
-        btnThreeDay.isEnabled = currentDay >= 3 && !dailyRewardManager.isRewardClaimedForDay(3)
-
-        if (dailyRewardManager.isRewardClaimedForDay(1)) {
-            btnFirstDay.setBackgroundResource(R.drawable.first_day_done_btn)
-            btnFirstDay.isEnabled = false
-            startActivity(Intent(this@DailyActivity, MenuActivity::class.java))
-            finish()
-        }
-
-        if (dailyRewardManager.isRewardClaimedForDay(2)) {
-            btnSecondDay.alpha = 1f
-            btnSecondDay.isEnabled = true
-            btnSecondDay.setBackgroundResource(R.drawable.second_day_done_btn)
-            startActivity(Intent(this@DailyActivity, MenuActivity::class.java))
-            finish()
-        }
-
-        if (dailyRewardManager.isRewardClaimedForDay(3)) {
-            btnThreeDay.alpha = 1f
-            btnThreeDay.isEnabled = true
-            btnThreeDay.setBackgroundResource(R.drawable.three_day_done_btn)
-            startActivity(Intent(this@DailyActivity, MenuActivity::class.java))
-            finish()
+    private fun markRewardShownToday() {
+        with(sharedPreferences.edit()) {
+            putString(
+                "lastRewardShownDate",
+                dateFormat.format(Date())
+            )
+            apply()
         }
     }
 
-    private fun claimReward(day: Int) {
-        dailyRewardManager.setRewardClaimedForDay(day)
-        dailyRewardManager.setLastRewardDay(day)
-        dailyRewardManager.setLastRewardDate(System.currentTimeMillis())
-        updateButtonStates()
+    private fun checkDailyReward() {
+        val lastRewardDay = sharedPreferences.getString("lastRewardDay", null)
+        val currentDate = dateFormat.format(Date())
 
-        val reward = dailyRewardManager.getRewardForDay(day)
+        if (lastRewardDay == null || lastRewardDay != currentDate) {
+            val currentDay = sharedPreferences.getInt("currentDay", 1)
+            if (currentDay > 3) {
+                resetRewardProgress()
+            } else {
+                with(sharedPreferences.edit()) {
+                    putString("lastRewardDay", currentDate)
+                    apply()
+                }
+            }
+        }
+    }
 
-        val prefs = getSharedPreferences("PrefDivinePower", MODE_PRIVATE)
-        val currentBalance = prefs.getString("balanceScore", "0")?.toIntOrNull() ?: 0
+    private fun resetRewardProgress() {
+        with(sharedPreferences.edit()) {
+            putInt("currentDay", 1)
+            putString("lastRewardDay", dateFormat.format(Date()))
+            putBoolean("day1Rewarded", false)
+            putBoolean("day2Rewarded", false)
+            putBoolean("day3Rewarded", false)
+            commit()
+        }
+    }
 
-        val newBalance = currentBalance + reward
-        prefs.edit()
-            .putString("balanceScore", newBalance.toString())
-            .apply()
+    private fun loadRewardProgress() {
+        val currentDay = sharedPreferences.getInt("currentDay", 1)
+        if (currentDay > 3) {
+            resetRewardProgress()
+        } else {
+            updateRewardUI(currentDay)
+        }
+    }
+
+    private fun updateRewardUI(currentDay: Int) {
+        updateButtonState(binding.btnFirstDay, 1, currentDay)
+        updateButtonState(binding.btnSecondDay, 2, currentDay)
+        updateButtonState(binding.btnThreeDay, 3, currentDay)
+    }
+
+    private fun updateButtonState(button: View, day: Int, currentDay: Int) {
+        val isRewarded = sharedPreferences.getBoolean("day${day}Rewarded", false)
+        button.isEnabled = day == currentDay && !isRewarded
+        button.alpha = if (day <= currentDay) 1f else 0.5f
+        Log.d("DailyActivity", "Day $day isRewarded: $isRewarded")
+        if (isRewarded) {
+            when (day) {
+                1 -> button.setBackgroundResource(R.drawable.first_day_done_btn)
+                2 -> button.setBackgroundResource(R.drawable.second_day_done_btn)
+                3 -> button.setBackgroundResource(R.drawable.three_day_done_btn)
+            }
+        } else {
+            when (day) {
+                1 -> button.setBackgroundResource(R.drawable.first_day_btn)
+                2 -> button.setBackgroundResource(R.drawable.second_day_btn)
+                3 -> button.setBackgroundResource(R.drawable.three_day_btn)
+            }
+        }
+    }
+
+    private fun collectReward(day: Int, button: View) {
+        val rewardAmount = when (day) {
+            1 -> 200
+            2 -> 500
+            3 -> 1000
+            else -> 0
+        }
+
+        val currentBalance = sharedPreferences.getString(
+            "balanceScores",
+            this.getString(R.string.text_default_balance)
+        )
+        var balance = stringToNumber(currentBalance.toString())
+        balance += rewardAmount
+        with(sharedPreferences.edit()) {
+            putString("balanceScores", balance.toString())
+            putBoolean("day${day}Rewarded", true)
+            apply()
+        }
+        markRewardShownToday()
+        updateButtonState(button, day, day)
+
+        val nextDay = day + 1
+        if (nextDay <= 3) {
+            with(sharedPreferences.edit()) {
+                putInt("currentDay", nextDay)
+                apply()
+            }
+        } else {
+            with(sharedPreferences.edit()) {
+                putInt("currentDay", 1)
+                apply()
+            }
+            resetRewardProgress()
+        }
+
+        goToMenu()
+    }
+
+    private fun goToMenu() {
+        startActivity(Intent(this, MenuActivity::class.java))
+        finish()
     }
 
     @Deprecated(
         "Deprecated in Java",
         ReplaceWith("super.onBackPressed()", "androidx.appcompat.app.AppCompatActivity")
     )
-    override fun onBackPressed() = super.onBackPressed()
+    override fun onBackPressed() {
+        super.onBackPressed()
+        goToMenu()
+    }
 }

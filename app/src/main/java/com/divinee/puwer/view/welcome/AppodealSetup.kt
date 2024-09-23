8e72ec916f96014d7d72ac9438ffba62eaae8086
Vlad <vladandryschenko98@gmail.com>
2024-09-23 15:56:21 +0300

    AppodealSetup: optimization initBanner, createInterstitialCallbacks, handleBannerFailure

--- app/src/main/java/com/divinee/puwer/view/welcome/AppodealSetup.kt
+++ app/src/main/java/com/divinee/puwer/view/welcome/AppodealSetup.kt
@@ -6,27 +6,28 @@ import com.appodeal.ads.InterstitialCallbacks
 
 object AppodealSetup {
     private var statusBannerShow = false
-    fun initBanner(activity: AppCompatActivity) {
-        Appodeal.setInterstitialCallbacks(object : InterstitialCallbacks {
+    fun initBanner(activity: AppCompatActivity) =
+        Appodeal.setInterstitialCallbacks(createInterstitialCallbacks(activity))
+
+    private fun createInterstitialCallbacks(activity: AppCompatActivity): InterstitialCallbacks {
+        return object : InterstitialCallbacks {
             override fun onInterstitialLoaded(isPrecache: Boolean) {
                 if (!statusBannerShow) Appodeal.show(activity, Appodeal.INTERSTITIAL)
             }
 
-            override fun onInterstitialFailedToLoad() =
-                (activity as MainActivity).navigateNotUseBanner()
-
+            override fun onInterstitialFailedToLoad() = handleBannerFailure(activity)
             override fun onInterstitialShown() {
                 statusBannerShow = true
             }
 
-            override fun onInterstitialShowFailed() =
-                (activity as MainActivity).navigateNotUseBanner()
-
+            override fun onInterstitialShowFailed() = handleBannerFailure(activity)
             override fun onInterstitialClosed() = (activity as MainActivity).checkNavigateToDaily()
-
             override fun onInterstitialClicked() {}
-
             override fun onInterstitialExpired() = Appodeal.cache(activity, Appodeal.INTERSTITIAL)
-        })
+        }
+    }
+
+    private fun handleBannerFailure(activity: AppCompatActivity) {
+        (activity as? MainActivity)?.navigateNotUseBanner()
     }
 }

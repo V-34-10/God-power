9cca76433d5e98fbe14286fb90af510e29820ebf
Vlad <vladandryschenko98@gmail.com>
2024-09-24 18:12:18 +0300

    VolumeManager: optimization: setVolume - merged setOn/setOff

--- app/src/main/java/com/divinee/puwer/view/settings/VolumeManager.kt
+++ app/src/main/java/com/divinee/puwer/view/settings/VolumeManager.kt
@@ -4,13 +4,15 @@ import android.content.Context
 import android.media.AudioManager
 
 class VolumeManager(context: Context) {
-    private val managerMusic by lazy { context.getSystemService(Context.AUDIO_SERVICE) as AudioManager }
-    private var defaultMusicVolume: Int = 10
-    fun setOnVolume() =
-        managerMusic.setStreamVolume(AudioManager.STREAM_MUSIC, defaultMusicVolume, 0)
+    private val audioManager = context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+    private var savedVolume: Int = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
 
-    fun setOffVolume() {
-        defaultMusicVolume = managerMusic.getStreamVolume(AudioManager.STREAM_MUSIC)
-        managerMusic.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0)
+    fun setVolume(on: Boolean) {
+        if (on) {
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, savedVolume, 0)
+        } else {
+            savedVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0)
+        }
     }
 }

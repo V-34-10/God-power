fb5e3998ee894e838b389db986c63b03f9129863
Vlad <vladandryschenko98@gmail.com>
2024-12-11 23:33:05 +0200

    GameOfferWall: fix add constructor, writeToParcel, describeContents; companion CREATOR: createFromParcel/newArray

--- app/src/main/java/com/divinee/puwer/models/GameOfferWall.kt
+++ app/src/main/java/com/divinee/puwer/models/GameOfferWall.kt
@@ -1,13 +1,37 @@
 package com.divinee.puwer.models
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class GameOfferWall(
     val itemIndex: Int? = 0,
     val menuLabel: String = "",
     val imageUrl: String = "",
     val backupVisual: String = "",
     val actionPrompt: String = "",
-) : Parcelable
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readValue(GameOfferWall::class.java.classLoader) as? Int,
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString()
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(itemIndex)
+        parcel.writeString(menuLabel)
+        parcel.writeString(imageUrl)
+        parcel.writeString(backupVisual)
+        parcel.writeString(actionPrompt)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<GameOfferWall> {
+
+        override fun createFromParcel(parcel: Parcel): GameOfferWall = GameOfferWall(parcel)
+
+        override fun newArray(size: Int): Array<GameOfferWall?> = arrayOfNulls(size)
+    }
+}

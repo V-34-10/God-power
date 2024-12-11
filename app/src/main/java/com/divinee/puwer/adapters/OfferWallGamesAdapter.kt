1a9ef581eafe4593cc100a51a9aa6a2708448e9c
Vlad <vladandryschenko98@gmail.com>
2024-12-11 17:03:25 +0200

    OfferWallGameAdapter: add change layout dependency with size listBanners

--- app/src/main/java/com/divinee/puwer/adapters/OfferWallGamesAdapter.kt
+++ app/src/main/java/com/divinee/puwer/adapters/OfferWallGamesAdapter.kt
@@ -10,16 +10,17 @@ import com.divinee.puwer.models.GameOfferWall
 class OfferWallGameAdapter(private val onClickList: (GameOfferWall) -> Unit) :
     RecyclerView.Adapter<OfferWallGameViewHolder>() {
     private var listGamesOfferWall: List<GameOfferWall> = emptyList()
-
+    private var layoutId = R.layout.game_offerwall_item
     @SuppressLint("NotifyDataSetChanged")
     fun updateGamesList(newListGames: List<GameOfferWall>) {
         listGamesOfferWall = newListGames
+        layoutId = if (newListGames.size > 3) R.layout.game_offerwall_item_grid else R.layout.game_offerwall_item
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OfferWallGameViewHolder =
         OfferWallGameViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.game_offerwall_item, parent, false)
+            LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
         )
 
     override fun onBindViewHolder(holder: OfferWallGameViewHolder, position: Int) =
